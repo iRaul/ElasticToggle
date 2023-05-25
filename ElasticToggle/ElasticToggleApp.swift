08e7d46edee396d54a094d74d297976eca65fb63
Raul <rauldronk@gmail.com>
2023-05-25 13:19:22 +0300

    Initial Commit

---
+++ ElasticToggle/ElasticToggleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElasticToggleApp.swift
+//  ElasticToggle
+//
+//  Created by Raul on 25.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct ElasticToggleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
